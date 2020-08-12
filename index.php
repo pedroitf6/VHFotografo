b7553c19b76b544c56219b45a24af688c74dce16
pedroitf6 <39974497+pedroitf6@users.noreply.github.com>
2020-08-12 13:37:55 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,306 @@
+<!doctype html>
+<html lang="pt-br">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Victor Hugo Fotografo - Obras de arte exclusivas a partir de fotografias impressas em material de alta qualidade.o</title>
+    <meta name="discription" content="Victor Hugo Fotografo - Obras de arte exclusivas a partir de fotografias impressas em material de alta qualidade.">
+    <meta name="keywords" content="fotografias, fotografias impressas, arte exclusiva, Fotografo REC">
+    <meta name="author" content="Pedro Tavares">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="img/FAVICON.png">
+
+  <!--  <link rel="stylesheet" href="css/stylesheet.css"> -->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/modernizr-2.6.2.min.js"></script>
+
+  	<script src="js/respond.min.js"></script>
+
+    <!-- Animate.css -->
+  	<link rel="stylesheet" href="css/animate.css">
+  	<!-- Icomoon Icon Fonts-->
+  	<link rel="stylesheet" href="css/icomoon.css">
+  	<!-- Simple Line Icons-->
+  	<link rel="stylesheet" href="css/simple-line-icons.css">
+  	<!-- Magnific Popup -->
+  	<link rel="stylesheet" href="css/magnific-popup.css">
+  	<!-- Owl Carousel -->
+  	<link rel="stylesheet" href="css/owl.carousel.min.css">
+  	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+  	<!-- Salvattore -->
+  	<link rel="stylesheet" href="css/salvattore.css">
+  	<!-- Theme Style -->
+  	<link rel="stylesheet" href="css/style.css">
+
+
+  </head>
+
+
+  <body>
+
+
+        <div id="fh5co-offcanvass">
+          <ul>
+            <li class="active"><a href="#" data-nav-section="home">MINHA CONTA</a></li>
+            <li><a href="#" data-nav-section="features">QUADROS</a></li>
+            <li><a href="#" data-nav-section="products">SOBRE</a></li>
+            <li><a href="#" data-nav-section="benefits">NOSSA GALERIA</a></li>
+            <li><a href="#" data-nav-section="pricing">VALORES E ACABAMENTOS</a></li>
+            <li><a href="#" data-nav-section="faqs">FALE CONOSCO</a></li>
+          </ul>
+        </div>
+
+        <div id="fh5co-menu" class="navbar">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#fh5co-navbar" aria-expanded="false" aria-controls="navbar"><span>Menu</span> <i></i></a>
+                <a href="index.html" class="navbar-brand"><span>Victor Hugo</span></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="fh5co-page">
+          <div id="fh5co-wrap">
+            <header id="fh5co-hero" data-section="home" role="banner" style="background: url(img/bg_2.jpg) top left; background-size: cover;" >
+              <div class="fh5co-overlay"></div>
+              <div class="fh5co-intro">
+                <div class="container">
+                  <div class="row">
+
+                    <div class="col-md-6 fh5co-text">
+                      <h2 class="to-animate intro-animate-1">Obras de arte exclusivas a partir de fotografias impressas em material de alta qualidade.</h2>
+                      <p class="to-animate intro-animate-2">Conheça agora nossos quadros !</p>
+                      <p class="to-animate intro-animate-3"><a href="#" class="btn btn-primary btn-md"><i class=""></i> Conheça agora !</a></p>
+                    </div>
+                    <div class="col-md-6 text-right fh5co-intro-img to-animate intro-animate-4">
+                      <img src="img/camera-digita1l.png" alt="">
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+            </header>
+
+            <!-- ________________________________END HEADER_________________________________-->
+
+            <div id="fh5co-main">
+              <div id="fh5co-clients">
+                <div class="container">
+                  <div class="row text-center">
+                     <div class="col-md-4 col-sm-7 col-xs-7 ">
+                    <div class=" icon-box featured-box icon-box-left text-left is-small" style="margin:px px px px;">
+                      <div class="icon-box-img" style="width: 42px">
+                        <div class="icon">
+                          <div class="icon-inner" style="color:rgb(143, 143, 143);">
+              						  <svg id="Layer_1" enable-background="new 0 0 512 512" height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><g><path d="m365.149 252.99c0-19.003-15.46-34.463-34.463-34.463s-34.463 15.46-34.463 34.463 15.46 34.463 34.463 34.463 34.463-15.46 34.463-34.463zm-53.926 0c0-10.732 8.731-19.463 19.463-19.463s19.463 8.731 19.463 19.463-8.731 19.463-19.463 19.463-19.463-8.731-19.463-19.463z"/><path d="m504.5 169.218c-15.765 0-28.59-12.825-28.59-28.59 0-4.142-3.358-7.5-7.5-7.5h-82.35l-97.208-58.93c1.046-3.289 1.611-6.791 1.611-10.423 0-19.003-15.46-34.463-34.463-34.463s-34.463 15.46-34.463 34.463c0 3.631.565 7.133 1.611 10.423l-97.208 58.93h-82.35c-4.142 0-7.5 3.358-7.5 7.5 0 15.765-12.825 28.59-28.59 28.59-4.142 0-7.5 3.358-7.5 7.5v262.381c0 4.142 3.358 7.5 7.5 7.5 15.765 0 28.59 12.825 28.59 28.59 0 4.142 3.358 7.5 7.5 7.5h348.462c4.142 0 7.5-3.358 7.5-7.5s-3.358-7.5-7.5-7.5h-341.608c-3.141-18.037-17.407-32.304-35.444-35.445v-248.672c18.037-3.141 32.303-17.408 35.444-35.444h411.111c3.141 18.037 17.408 32.303 35.444 35.444v248.672c-18.037 3.141-32.303 17.408-35.444 35.444h-35.083c-4.142 0-7.5 3.358-7.5 7.5s3.358 7.5 7.5 7.5h41.937c4.142 0 7.5-3.358 7.5-7.5 0-15.765 12.825-28.59 28.59-28.59 4.142 0 7.5-3.358 7.5-7.5v-262.38c.001-4.142-3.357-7.5-7.499-7.5zm-248.5-124.906c10.732 0 19.463 8.731 19.463 19.463s-8.731 19.463-19.463 19.463-19.463-8.731-19.463-19.463 8.731-19.463 19.463-19.463zm-25.295 42.846c6.3 6.81 15.308 11.08 25.295 11.08s18.995-4.27 25.294-11.08l75.83 45.97h-202.25z"/><path d="m70.918 183.128h23.205c4.142 0 7.5-3.358 7.5-7.5s-3.358-7.5-7.5-7.5h-27.337c-2.651 0-5.105 1.399-6.455 3.681-5.265 8.898-12.751 16.385-21.65 21.65-2.282 1.35-3.681 3.804-3.681 6.455v215.988c0 2.651 1.399 5.105 3.681 6.455 8.899 5.265 16.386 12.751 21.65 21.65 1.35 2.282 3.804 3.681 6.455 3.681h378.427c2.651 0 5.105-1.399 6.455-3.681 5.264-8.899 12.751-16.385 21.65-21.65 2.282-1.35 3.681-3.804 3.681-6.455v-215.988c0-2.651-1.399-5.105-3.681-6.455-8.899-5.265-16.385-12.751-21.65-21.65-1.35-2.282-3.804-3.681-6.455-3.681h-316.412c-4.142 0-7.5 3.358-7.5 7.5s3.358 7.5 7.5 7.5h312.28c5.541 8.248 12.67 15.377 20.918 20.918v152.524l-62.965-62.965c-1.406-1.407-3.314-2.197-5.303-2.197s-3.897.79-5.303 2.197l-57.742 57.742-107.742-107.743c-2.929-2.929-7.678-2.929-10.606 0l-54.383 54.383-34.383-34.383c-1.406-1.407-3.314-2.197-5.303-2.197s-3.897.79-5.303 2.197l-62.966 62.966v-122.524c8.249-5.541 15.377-12.67 20.918-20.918zm47.35 96.386 56.028 56.028c1.464 1.464 3.384 2.197 5.303 2.197s3.839-.732 5.303-2.197c2.929-2.929 2.929-7.678 0-10.606l-16.341-16.341 49.08-49.08 107.742 107.742 22.39 22.391c1.464 1.465 3.384 2.197 5.303 2.197s3.839-.732 5.303-2.197c2.929-2.929 2.929-7.678 0-10.606l-17.087-17.087 52.439-52.439 68.269 68.267v33.987c-8.249 5.542-15.378 12.67-20.918 20.919h-370.163c-5.541-8.249-12.67-15.378-20.919-20.919v-63.987z"/></g>
+                            </svg>
+                          </div>
+            				        </div>
+            			           </div>
+                             <div class="icon-box-text last-reset">
+                               <p>ESCOLHA SEU QUADRO E ENVIE PARA O CARRINHO.</p>
+                             </div>
+                           </div>
+                         </div>
+                           <div class="col-md-4 col-sm-7 col-xs-7">
+                           <div class=" icon-box featured-box icon-box-left text-left is-small" style="margin:px px px px;">
+                             <div class="icon-box-img" style="width: 42px">
+                               <div class="icon">
+                                 <div class="icon-inner" style="color:rgb(143, 143, 143);">
+                                   <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                   	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g><g>
+                                       <path d="M458.667,85.333H53.333C23.936,85.333,0,109.269,0,138.667v234.667c0,29.397,23.936,53.333,53.333,53.333h405.333
+                                       c29.397,0,53.333-23.936,53.333-53.333V138.667C512,109.269,488.064,85.333,458.667,85.333z M490.667,373.333
+                                   		  c0,17.643-14.357,32-32,32H53.333c-17.643,0-32-14.357-32-32V138.667c0-17.643,14.357-32,32-32h405.333c17.643,0,32,14.357,32,32
+                                   			V373.333z"/></g></g><g><g>
+                                   		<path d="M501.333,149.333H10.667C4.779,149.333,0,154.112,0,160v64c0,5.888,4.779,10.667,10.667,10.667h490.667
+                                   			c5.888,0,10.667-4.779,10.667-10.667v-64C512,154.112,507.221,149.333,501.333,149.333z M490.667,213.333H21.333v-42.667h469.333
+                                   			V213.333z"/></g></g><g><g>
+                                   		<path d="M202.667,298.667h-128c-5.888,0-10.667,4.779-10.667,10.667S68.779,320,74.667,320h128
+                                   			c5.888,0,10.667-4.779,10.667-10.667S208.555,298.667,202.667,298.667z"/></g></g><g><g>
+                                   		<path d="M202.667,341.333h-128C68.779,341.333,64,346.112,64,352c0,5.888,4.779,10.667,10.667,10.667h128
+                                   			c5.888,0,10.667-4.779,10.667-10.667C213.333,346.112,208.555,341.333,202.667,341.333z"/></g></g><g><g>
+                                   		<path d="M416,277.333h-21.333c-17.643,0-32,14.357-32,32v21.333c0,17.643,14.357,32,32,32H416c17.643,0,32-14.357,32-32v-21.333
+                                   			C448,291.691,433.643,277.333,416,277.333z M426.667,330.667c0,5.888-4.779,10.667-10.667,10.667h-21.333
+                                   			c-5.888,0-10.667-4.779-10.667-10.667v-21.333c0-5.888,4.779-10.667,10.667-10.667H416c5.888,0,10.667,4.779,10.667,10.667
+                                   			V330.667z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+                                      </svg>
+                                 </div>
+                   				        </div>
+                   			           </div>
+                                    <div class="icon-box-text last-reset">
+                                      <p>CADASTRE-SE E FINALIZE O PAGAMENTO NO CARTÃO.</p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="col-md-4 col-sm-7 col-xs-7">
+                                <div class=" icon-box featured-box icon-box-left text-left is-small" style="margin:px px px px;">
+                                  <div class="icon-box-img" style="width: 42px">
+                                    <div class="icon">
+                                      <div class="icon-inner" style="color:rgb(143, 143, 143);">
+                                        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                        	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g><g>
+                                        		<path d="M491.729,112.971L259.261,0.745c-2.061-0.994-4.461-0.994-6.521,0L20.271,112.971c-2.592,1.251-4.239,3.876-4.239,6.754
+                                        			v272.549c0,2.878,1.647,5.503,4.239,6.754l232.468,112.226c1.03,0.497,2.146,0.746,3.261,0.746s2.23-0.249,3.261-0.746
+                                        			l232.468-112.226c2.592-1.251,4.239-3.876,4.239-6.754V119.726C495.968,116.846,494.32,114.223,491.729,112.971z M256,15.828
+                                        			l215.217,103.897l-62.387,30.118c-0.395-0.301-0.812-0.579-1.27-0.8L193.805,45.853L256,15.828z M176.867,54.333l214.904,103.746
+                                        			l-44.015,21.249L132.941,75.624L176.867,54.333z M396.799,172.307v78.546l-41.113,19.848v-78.546L396.799,172.307z
+                                        			 M480.968,387.568L263.5,492.55V236.658l51.873-25.042c3.73-1.801,5.294-6.284,3.493-10.015
+                                        			c-1.801-3.729-6.284-5.295-10.015-3.493L256,223.623l-20.796-10.04c-3.731-1.803-8.214-0.237-10.015,3.493
+                                        			c-1.801,3.73-0.237,8.214,3.493,10.015l19.818,9.567V492.55L31.032,387.566V131.674l165.6,79.945
+                                        			c1.051,0.508,2.162,0.748,3.255,0.748c2.788,0,5.466-1.562,6.759-4.241c1.801-3.73,0.237-8.214-3.493-10.015l-162.37-78.386
+                                        			l74.505-35.968L340.582,192.52c0.033,0.046,0.07,0.087,0.104,0.132v89.999c0,2.581,1.327,4.98,3.513,6.353
+                                        			c1.214,0.762,2.599,1.147,3.988,1.147c1.112,0,2.227-0.247,3.26-0.746l56.113-27.089c2.592-1.251,4.239-3.875,4.239-6.754v-90.495
+                                        			l69.169-33.392V387.568z"/></g></g><g><g>
+                                        		<path d="M92.926,358.479L58.811,342.01c-3.732-1.803-8.214-0.237-10.015,3.493c-1.801,3.73-0.237,8.214,3.493,10.015
+                                        			l34.115,16.469c1.051,0.508,2.162,0.748,3.255,0.748c2.788,0,5.466-1.562,6.759-4.241
+                                        			C98.22,364.763,96.656,360.281,92.926,358.479z"/></g></g><g><g>
+                                        		<path d="M124.323,338.042l-65.465-31.604c-3.731-1.801-8.214-0.237-10.015,3.494c-1.8,3.73-0.236,8.214,3.494,10.015
+                                        			l65.465,31.604c1.051,0.507,2.162,0.748,3.255,0.748c2.788,0,5.466-1.562,6.759-4.241
+                                        			C129.617,344.326,128.053,339.842,124.323,338.042z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+                                        </svg>
+                                      </div>
+                                       </div>
+                                        </div>
+                                         <div class="icon-box-text last-reset">
+                                           <p>RECEBA NO LOCAL ESCOLHIDO.</p>
+                                         </div>
+                                       </div>
+                                     </div>
+                                   </div>
+                                 </div>
+                               </div>
+
+
+
+
+
+
+
+
+
+  <div class="anuncios">
+  <div class="anuncio">
+      <h3 class="section-title section-title-center"><b></b><span class="section-title-main">PERNAMBUCO SPECIAL EDITION</span><b></b></h3>
+    <div class="ind-anuncio">
+        <div class="ind-anuncio-image"><img src="img/05-600x750.jpg" alt=""></div>
+        <div class="ind-anuncio-texto"><br>COISA DE DEUS – QUADRO NORONHA – PAPEL – 0,45X0,65 m
+        <div class="ind-anuncio-valor"><br>R$ 900.00</div></div>
+    </div>
+
+    <div class="ind-anuncio">
+        <div class="ind-anuncio-image"><img src="img/05-600x750.jpg" alt=""></div>
+        <div class="ind-anuncio-texto"><br>COISA DE DEUS – QUADRO NORONHA – PAPEL – 0,45X0,65 m
+        <div class="ind-anuncio-valor"><br>R$ 900.00</div></div>
+    </div>
+
+    <div class="ind-anuncio">
+        <div class="ind-anuncio-image"><img src="img/05-600x750.jpg" alt=""></div>
+        <div class="ind-anuncio-texto"><br>COISA DE DEUS – QUADRO NORONHA – PAPEL – 0,45X0,65 m
+        <div class="ind-anuncio-valor"><br>R$ 900.00</div></div>
+    </div>
+
+    <div class="ind-anuncio">
+        <div class="ind-anuncio-image"><img src="img/05-600x750.jpg" alt=""></div>
+        <div class="ind-anuncio-texto"><br>COISA DE DEUS – QUADRO NORONHA – PAPEL – 0,45X0,65 m
+        <div class="ind-anuncio-valor"><br>R$ 900.00</div></div>
+    </div>
+  </div>
+ </div>
+
+
+ <!--<div class="anuncio"></div>
+
+   <div class="anuncios">
+
+ <div class="anuncio">
+     <h3 class="section-title section-title-center"><b></b><span class="section-title-main">PERNAMBUCO SPECIAL EDITION</span><b></b></h3>
+   <div class="ind-anuncio">
+       <div class="ind-anuncio-image"><img src="img/05-600x750.jpg" alt=""></div>
+       <div class="ind-anuncio-texto"><br>COISA DE DEUS – QUADRO NORONHA – PAPEL – 0,45X0,65 m
+       <div class="ind-anuncio-valor"><br>R$ 900.00</div></div>
+   </div>
+
+   <div class="ind-anuncio">
+       <div class="ind-anuncio-image"><img src="img/05-600x750.jpg" alt=""></div>
+       <div class="ind-anuncio-texto"><br>COISA DE DEUS – QUADRO NORONHA – PAPEL – 0,45X0,65 m
+       <div class="ind-anuncio-valor"><br>R$ 900.00</div></div>
+   </div>
+
+   <div class="ind-anuncio">
+       <div class="ind-anuncio-image"><img src="img/05-600x750.jpg" alt=""></div>
+       <div class="ind-anuncio-texto"><br>COISA DE DEUS – QUADRO NORONHA – PAPEL – 0,45X0,65 m
+       <div class="ind-anuncio-valor"><br>R$ 900.00</div></div>
+   </div>
+
+   <div class="ind-anuncio">
+       <div class="ind-anuncio-image"><img src="img/05-600x750.jpg" alt=""></div>
+       <div class="ind-anuncio-texto"><br>COISA DE DEUS – QUADRO NORONHA – PAPEL – 0,45X0,65 m
+       <div class="ind-anuncio-valor"><br>R$ 900.00</div></div>
+   </div>
+ </div>
+</div>
+-->
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- jQuery -->
+  	<script src="js/jquery.min.js"></script>
+  	<!-- jQuery Easing -->
+  	<script src="js/jquery.easing.1.3.js"></script>
+  	<!-- Bootstrap -->
+  	<script src="js/bootstrap.min.js"></script>
+  	<!-- Waypoints -->
+  	<script src="js/jquery.waypoints.min.js"></script>
+  	<!-- Magnific Popup -->
+  	<script src="js/jquery.magnific-popup.min.js"></script>
+  	<!-- Owl Carousel -->
+  	<script src="js/owl.carousel.min.js"></script>
+  	<!-- toCount -->
+  	<script src="js/jquery.countTo.js"></script>
+  	<!-- Main JS -->
+  	<script src="js/main.js"></script>
+
+
+
+
+
+  </body>
+
+
+
+
+</html>
